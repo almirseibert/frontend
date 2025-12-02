3e5121b6a5aded8468199b670bb10f9fbd9a8037
almirseibert <almir.seibert@gmail.com>
2025-12-02 08:36:40 -0300

    Abastecimento

--- src/components/modals/RefuelingOrderModal.js
+++ src/components/modals/RefuelingOrderModal.js
@@ -184,7 +184,7 @@ const RefuelingOrderModal = ({
                 }
 
                 if (diff > 0 && litros > 0) {
-                    const avg = unit === 'Km/L' ? (diff / litros) : (litros / diff);
+                    const avg = unit === 'Km/L' ? (diff / liters) : (liters / diff);
                     setLastAverage(`${avg.toFixed(2)} ${unit}`);
                 } else {
                     setLastAverage('Incalculável');
